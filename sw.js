976a133e6e1f37800686e0151a008bdd12b48eb9
fauzi-svg <fauziahmadnurjaman10@gmail.com>
2026-05-05 16:07:12 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'cv-pwa-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/app.js'
+];
+
+// Install: Simpan file ke cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets...');
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch: Ambil dari cache jika offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate: Hapus cache lama jika ada update
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
